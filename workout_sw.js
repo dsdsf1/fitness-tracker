3221ce83aeca5fbbead0a0441c2508993de5766b
dsdsf1 <3337442484@qq.com>
2026-06-21 16:13:07 +0800

    🏋️ 囚徒健身 PWA - 居家增肌塑形训练计划

---
+++ workout_sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'workout-v3';
+const ASSETS = [
+  '/',
+  'index.html',
+  'workout_manifest.json',
+  'workout_icon_192.png',
+  'workout_icon_512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('index.html');
+      }
+    })
+  );
+});
